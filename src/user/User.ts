ead049ea0833dc42e4bc831ceb97ca812b02545e
omar-a-eid <omerx9988@gmail.com>
2024-05-23 14:23:13 +0300

    Feat: Add User class with properties

---
+++ src/user/User.ts
@@ -0,0 +1,20 @@
+export class User {
+  private id: number;
+  private name: string;
+  
+  constructor(
+    id: number,
+    name: string
+  ) {
+    this.id = id;
+    this.name = name;
+  }
+
+  getId(): number {
+      return this.id;
+  }
+
+  getName(): string {
+      return this.name;
+  }
+}
