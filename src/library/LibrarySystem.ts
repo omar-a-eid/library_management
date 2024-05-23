5a0d3cae8cd6383ebf1d4b527fbaa0bdf95c559d
omar-a-eid <omerx9988@gmail.com>
2024-05-23 15:23:37 +0300

    Feat: Add LibaraySystem Class with all the behaviors and state

---
+++ src/library/LibrarySystem.ts
@@ -0,0 +1,52 @@
+import { Book } from "../book";
+import { User } from "../user";
+import { Library } from "./Library";
+
+export class LibrarySystem implements Library {
+  private books: Book[] = [];
+  private users: User[] = [];
+
+  addBook(book: Book): void {
+    this.books.push(book);
+  }
+  
+  removeBook(isbn: string): void {
+    this.books = this.books.filter(book => book.getIsbn() !== isbn);
+  }
+
+  findBook(isbn: string): Book | undefined {
+    return this.books.find(book => book.getIsbn() === isbn);
+  }
+
+  borrowBook(isbn: string, user: User): void {
+    const book = this.findBook(isbn);
+
+    if(!book) {
+      throw new Error(`Book with ISBN ${isbn} not found`);
+    }
+
+    book.borrowBook(user);
+  }
+
+  returnBook(isbn: string, user: User): void {
+    const book = user.getBorrowedBooks().find(book => book.getIsbn() === isbn);
+
+    if (!book) {
+      throw new Error(`${user.getName()} does not have this book borrowed`);
+    }
+
+    book.returnBook(user);
+  }
+
+  registerUser(user: User): void {
+    this.users.push(user);
+  }
+
+  unregisterUser(userId: number): void {
+    this.users = this.users.filter(user => user.getId() !== userId);
+  }
+
+  findUser(userId: number): User | undefined {
+    return this.users.find(user => user.getId() === userId);
+  }
+}
