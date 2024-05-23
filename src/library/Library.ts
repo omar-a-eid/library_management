1201047bb3283334c86e72e376c3dfc54d4429f6
omar-a-eid <omerx9988@gmail.com>
2024-05-23 15:12:21 +0300

    Feat: Add methods to register/unregister Users, find user and borrow/return books

--- src/library/Library.ts
+++ src/library/Library.ts
@@ -1,7 +1,13 @@
 import { Book } from "../book";
+import { User } from "../user";
 
 export interface Library {
   addBook(book: Book): void;
   removeBook(isbn: string): void;
   findBook(isbn: string): Book | undefined;
+  borrowBook(isbn:string, user: User): void;
+  returnBook(isbn: string, user: User): void;
+  registerUser(userId: number): void;
+  unregisterUser(userId: number): void;
+  findUser(userId: number): User | undefined;
 }
