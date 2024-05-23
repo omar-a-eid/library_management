5caa1323bcf509e6ff56c925f3cf6a327106466c
omar-a-eid <omerx9988@gmail.com>
2024-05-23 14:16:15 +0300

    Feat: Add Book class with properties

---
+++ src/book/Book.ts
@@ -0,0 +1,34 @@
+export class Book{
+    private title: string;
+    private author: string;
+    private isbn: string;
+    private availability: boolean;
+
+    constructor(
+        title: string,
+        author: string,
+        isbn: string,
+        availability: boolean = true
+    ) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.availability = availability;
+    }
+
+    getTitle(): string {
+        return this.title;
+    }
+
+    getAuthor(): string {
+        return this.author;
+    }
+
+    getIsbn(): string {
+        return this.isbn;
+    }
+
+    isAvailable(): boolean {
+        return this.availability;
+    }
+}
