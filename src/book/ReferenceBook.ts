3cd578bc2386ddef015014cff7e1bce96861b4f4
omar-a-eid <omerx9988@gmail.com>
2024-05-23 14:59:15 +0300

    Feat: override the borrow and return methods for ReferenceBook Class

--- src/book/ReferenceBook.ts
+++ src/book/ReferenceBook.ts
@@ -1,3 +1,4 @@
+import { User } from "../user";
 import { Book } from "./Book";
 
 export class ReferenceBook extends Book {
@@ -7,5 +8,13 @@ export class ReferenceBook extends Book {
     isbn: string
   ) {
     super(title, author, isbn);
+  }
+
+  borrowBook(user: User): void {
+    console.log(`${this.getTitle()} cannot be borrowed as it is a reference book.`);
+  }
+
+  returnBook(user: User): void {
+    console.log(`${this.getTitle()} is a reference book and cannot be returned.`);
   }
 }
